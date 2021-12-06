2ebccffc9475fc229c125c0aefc025eca2212abb
ronaldodeveloper <rgm.frontend@hotmail.com>
2021-12-06 11:38:26 -0300

    update

--- array.js
+++ array.js
@@ -58,9 +58,40 @@ let juntar2= arrayStr.join(' ')
 
 
 // 8. Escreva um programa JavaScript para encontrar o item mais frequente de um array.
-let arrItem= [54,12,58,6,36,6,54,84,1,7,36,54,12,74,14,9,1,48,58,84,12,54]
-let maisFrequente= arrItem.filter((el, ind)=>{
-    
-    return el === (ind)
+let itensMaisFrequentes= [54,12,58,6,36,6,54,84,1,7,36,54,12,74,14,9,1,48,58,84,12,54]
+let ordenarLista= itensMaisFrequentes.sort((a,b)=>{
+    return a>b
 })
-console.log(maisFrequente) 
+// console.log(ordenarLista) // INCOMPLETO!
+
+// 11. Escreva um programa JavaScript para encontrar a soma dos quadrados de um vetor numérico.
+let vetor=[54,7,11,86,9]
+let somaDosQuadradosItems= vetor.map((el)=>{
+   return el**2
+})
+let somaDosQuadradosTotal= vetor.reduce((acc, cur)=>{
+    let total = acc + cur;
+    let quad= Math.pow(total, 2)
+    return quad
+})
+// console.log(somaDosQuadradosItems)
+// console.log(somaDosQuadradosTotal)
+
+//12. Escreva um programa JavaScript para calcular a soma e o produto de um array de inteiros
+function somar(a, b){
+    let soma= a + b;
+    let prod= a * b;
+
+    return { soma, prod }
+}
+console.log(somar(15,35))
+// ou
+let somaEproduto= [46,1,88,59,11,3]
+let calcular= somaEproduto.reduce((acc,cur)=>{
+    let soma= acc + cur;
+    let prod= acc * cur;
+    return prod
+})
+// console.log(calcular) 
+
+
