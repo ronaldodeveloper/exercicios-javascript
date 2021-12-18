54f2b1f68a0148f959007ac88dec0a7c4ed877cb
Ronaldo Mariano <rgm.frontend@hotmail.com>
2021-12-17 22:37:45 -0300

    update

--- js/data.js
+++ js/data.js
@@ -1,1 +1,18 @@
-console.log(new Date())
+/*
+##### DATA  
+## ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
+*/
+
+
+// exercises
+const data= new Date()  // base date
+
+ // 2. Escreva uma função JavaScript para obter a data atual.  
+const formatarData= Intl.DateTimeFormat('pt',{
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+})
+console.log(formatarData.format(data))
+
+// 3. Escreva uma função JavaScript para obter o número de dias em um mês.
