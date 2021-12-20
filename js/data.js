7be909dee27b57af2c107449104fe28e63604d6d
ronaldodeveloper <rgm.frontend@hotmail.com>
2021-12-20 14:17:08 -0300

    update

--- js/data.js
+++ js/data.js
@@ -13,6 +13,38 @@ const formatarData= Intl.DateTimeFormat('pt',{
     month: 'long',
     year: 'numeric'
 })
-console.log(formatarData.format(data))
+// onsole.log(formatarData.format(data))
 
 // 3. Escreva uma função JavaScript para obter o número de dias em um mês.
+
+
+// 18. Escreva um programa JavaScript para calcular a idade.
+const dataNascimento= new Date(20/12/1965)
+const dataAtual= new Date()
+const idade= Intl.DateTimeFormat('pt-BR', {
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
+})
+//onsole.log(idade.format(dataNascimento));
+//console.log(idade.format(dataAtual));
+const res= ' ';
+console.log(res);
+
+
+
+// crie um relógio
+function relogio(){
+    let clock= document.querySelector(".clock") 
+    let atual= new Date();
+    let tempo = Intl.DateTimeFormat('pr-BR', {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    })
+    let current= tempo.format(atual)
+    clock.textContent= current
+
+    setTimeout(relogio, 1000)  
+}
+relogio()
